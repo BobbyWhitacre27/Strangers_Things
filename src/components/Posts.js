db663de94546489c32486de24b57463af39351aa
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2022-12-20 22:07:50 -0500

    Delete Posts is working with bugs. Must hit delete button 3x. Working on bug fix.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const Posts = ({ user }) => {
     const [posts, setPosts] = useState([])
+    const [POST_ID, setPOST_ID] = useState('');
 
     const fetchPosts = async () => {
         try {
@@ -12,8 +13,7 @@ const Posts = ({ user }) => {
                         'Content-Type': 'application/json',
                         'Authorization': `Bearer ${user.token}`
                     }
-                })
-                ;
+                });
             const apiPosts = await response.json();
             console.log(apiPosts)
             setPosts(apiPosts.data.posts);
@@ -21,6 +21,31 @@ const Posts = ({ user }) => {
             console.log('err', err)
         }
     }
+
+    const handleDelete = (event) => {
+        setPOST_ID(event.target.value)
+        deletePosts()
+        fetchPosts()
+    }
+
+    const deletePosts = async () => {
+        try {
+            console.log(POST_ID)
+            const response = await fetch(`https://strangers-things.herokuapp.com/api/2209-ftb-ct-web-pt/posts/${POST_ID}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${user.token}`
+                    }
+                });
+            const deletePost = await response.json();
+            console.log(deletePost)
+        } catch (err) {
+            console.log('err', err)
+        }
+    }
+
 
     useEffect(() => {
         fetchPosts();
@@ -43,7 +68,7 @@ const Posts = ({ user }) => {
                             <ul id='postedby'>Posted by: {e.author.username} {e.createdAt}</ul>
                             <ul id='postdelivery'>{e.willDeliver === true ? 'Will Deliver' : "Will Not Deliver"}</ul>
                         </div>
-                        {JSON.stringify(e.isAuthor) === 'true' ? <div id='yourPost'>Posted by you: <span id='usernameSpan'>{user.username}</span>.</div> : ''}
+                        {JSON.stringify(e.isAuthor) === 'true' ? <div id='yourPostDiv'><div id='yourPostInfo'>Posted by you: <span id='usernameSpan'>{user.username}</span></div><button onClick={handleDelete} value={e._id} id='deleteButton'>Delete</button></div> : ''}
                     </div>)
                 })}
             </div>
